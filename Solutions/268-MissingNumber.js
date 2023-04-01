429423a5f62503e54feefc8e1f5438cbf109550a
Jagrut Sharma <jagrut.sharma1@gmail.com>
2023-04-01 11:42:07 +0530

    update solution

--- Solutions/268-MissingNumber.js
+++ Solutions/268-MissingNumber.js
@@ -15,20 +15,33 @@
 //   }
 // };
 
+// const missingNumber = function (nums) {
+//   let numsObj = {};
+//   let isPresent;
+//   for (let i = 0; i < nums.length; i++) {
+//     numsObj[nums[i]] = nums[i];
+//   }
+//   console.log(numsObj);
+//   const keysNumsObj = Object.keys(numsObj);
+//   // console.log(keysNumsObj);
+//   for (let j = 0; j <= keysNumsObj.length; j++) {
+//     // console.log(j, +keysNumsObj[j]);
+//     if (j !== +keysNumsObj[j]) return j;
+//   }
+// };
+
 const missingNumber = function (nums) {
-  let numsObj = {};
-  let isPresent;
+  const n = nums.length;
+  const expectedSum = (n * (n + 1)) / 2;
+  let actualSum = 0;
+
   for (let i = 0; i < nums.length; i++) {
-    numsObj[nums[i]] = nums[i];
+    actualSum = actualSum + nums[i];
   }
-  //   console.log(numsObj);
-  const keysNumsObj = Object.keys(numsObj);
-  //   console.log(keysNumsObj);
-  for (let j = 0; j < keysNumsObj.length; j++) {
-    // console.log(j, +keysNumsObj[j]);
-    if (j !== +keysNumsObj[j]) return j;
-  }
+
+  return expectedSum - actualSum;
 };
 
 console.log(missingNumber([0, 3, 1]));
 console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]));
+console.log(missingNumber([0, 1]));
