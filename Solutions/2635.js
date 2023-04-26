3008793c0c32e6bd46076d8f7883cf3fe002e6e1
Jagrut Sharma <jagrut.sharma1@gmail.com>
2023-04-26 16:24:03 +0530

    2635

---
+++ Solutions/2635.js
@@ -0,0 +1,12 @@
+const map = function (arr, fn) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = fn(arr[i], i);
+  }
+  return arr;
+};
+
+console.log(
+  map([1, 2, 3], function plusone(n) {
+    return n + 1;
+  })
+);
