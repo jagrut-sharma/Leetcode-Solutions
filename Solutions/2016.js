5220eb12c0572e061876827bc82f4f1e2f18933d
Jagrut Sharma <jagrut.sharma1@gmail.com>
2023-05-06 13:53:23 +0530

    2016

--- Solutions/2016.js
+++ Solutions/2016.js
@@ -1,30 +1,33 @@
 // Link: https://leetcode.com/problems/maximum-difference-between-increasing-elements/
 
+// Optimised:
+
 const maximumDifference = function (nums) {
-  let largestNum = Number.NEGATIVE_INFINITY;
-  let shortestNum = Number.POSITIVE_INFINITY;
-
-  let shortIndx, largeIndx;
-
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] > largestNum) {
-      largestNum = nums[i];
-      largeIndx = i;
-    }
-    if (nums[i] < shortestNum) {
-      shortestNum = nums[i];
-      shortIndx = i;
-    }
+  var min = Number.POSITIVE_INFINITY;
+  var diff = -1;
+  console.log(nums);
+  for (i = 0; i < nums.length; i++) {
+    min = Math.min(min, nums[i]);
+    diff = Math.max(diff, nums[i] - min);
+    console.log(min, diff);
   }
-
-  console.log(largestNum, largeIndx);
-  console.log(shortestNum, shortIndx);
-
-  if (shortIndx < largeIndx) return largestNum - shortestNum;
-
-  return -1;
+  return diff == 0 ? -1 : diff;
 };
 
-console.log(maximumDifference([7, 1, 3, 5, 4]));
-// console.log(maximumDifference([9, 4, 3, 2]));
-// console.log(maximumDifference([1, 5, 2, 10]));
+// const maximumDifference = function (nums) {
+//   var diff = -1;
+//   console.log(nums);
+//   for (let i = 0; i < nums.length; i++) {
+//     console.log("i=", nums[i]);
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[j] > nums[i]) {
+//         console.log(nums[j], nums[i]);
+//         diff = Math.max(nums[j] - nums[i], diff);
+//       }
+//     }
+//     console.log(diff);
+//   }
+//   return diff;
+// };
+
+console.log(maximumDifference([87, 68, 91, 86, 58, 63, 43, 98, 6, 40])); // 6 -> 8th, 98 -> 7th ==> 40 -> 9th(makes no sense as its last) => 43 -> 6th, 98 -> 7th => 98-43 = 55
