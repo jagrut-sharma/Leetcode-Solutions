b4a63d7957d579ddb41ceb7acb7f3d63ec5611aa
Jagrut Sharma <jagrut.sharma1@gmail.com>
2023-05-07 15:52:01 +0530

    2413

--- Solutions/2016.js
+++ Solutions/2016.js
@@ -3,16 +3,17 @@
 // Optimised:
 
 const maximumDifference = function (nums) {
-  var min = Number.POSITIVE_INFINITY;
-  var diff = -1;
-  console.log(nums);
-  for (i = 0; i < nums.length; i++) {
-    min = Math.min(min, nums[i]);
+  let min = Number.POSITIVE_INFINITY;
+  let diff = -1;
+  for (let i = 0; i < nums.length; i++) {
+    min = Math.min(nums[i], min);
     diff = Math.max(diff, nums[i] - min);
-    console.log(min, diff);
   }
-  return diff == 0 ? -1 : diff;
+
+  return diff === 0 ? -1 : diff;
 };
+
+console.log(maximumDifference([87, 68, 91, 86, 58, 63, 43, 98, 6, 40]));
 
 // const maximumDifference = function (nums) {
 //   var diff = -1;
@@ -30,4 +31,4 @@ const maximumDifference = function (nums) {
 //   return diff;
 // };
 
-console.log(maximumDifference([87, 68, 91, 86, 58, 63, 43, 98, 6, 40])); // 6 -> 8th, 98 -> 7th ==> 40 -> 9th(makes no sense as its last) => 43 -> 6th, 98 -> 7th => 98-43 = 55
+// console.log(maximumDifference([87, 68, 91, 86, 58, 63, 43, 98, 6, 40])); // 6 -> 8th, 98 -> 7th ==> 40 -> 9th(makes no sense as its last) => 43 -> 6th, 98 -> 7th => 98-43 = 55
